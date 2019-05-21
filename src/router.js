6e4b7e6794538002dba8009297c036aa4ba6febd
kgoel085 <kgoel085@gmail.com>
2019-05-22 01:13:22 +0530

    Added Token auth guard for each route

--- src/router.js
+++ src/router.js
@@ -1,10 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import store from './store'
 
 Vue.use(Router)
 
-export default new Router({
+// Generates a new API token
+const generateToken = async () => {
+  await store.dispatch('generateToken');
+};
+
+// Check if token is expired or not , if yes, clear previous one and get new one
+const checkToken = async() => {
+  let tmpVars = (localStorage.getItem('jTokn')) ? JSON.parse(atob(localStorage.getItem('jTokn').split('.')[1])) : false;
+
+  if(tmpVars){
+    //Check the expiry time
+    let currentTime = (new Date().getTime() / 1000).toFixed(0);
+
+    // If expired
+    if(currentTime > tmpVars.exp){
+      //Remove current & generate new one
+      store.commit('removeToken');
+      await generateToken();
+    }
+  }
+};
+
+let router =  new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -23,3 +46,15 @@ export default new Router({
     }
   ]
 })
+
+// Checks whether API token is valid or not before going to a route
+router.beforeEach((to, from, nxt) => {
+  let usrToken = localStorage.getItem('jTokn');
+  
+  if(!usrToken) generateToken();
+  else checkToken();
+
+  nxt();
+});
+
+export default router
