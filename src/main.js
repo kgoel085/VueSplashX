78a8e8ea7c524ca61528b5c6236d51cf682fc7df
kgoel085 <kgoel085@gmail.com>
2019-05-21 23:29:13 +0530

    Added axios and its configurations

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+//Axios default configurations
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+axios.defaults.headers.common['Accepts'] = 'application/json';
 
 Vue.config.productionTip = false
 
