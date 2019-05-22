03479927f379999da632eb322e4c93dd768569a3
kgoel085 <kgoel085@gmail.com>
2019-05-23 00:42:06 +0530

    Added vuetify theme configurations

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
+  theme: {
+    primary: '#1867c0',
+    secondary: '#7bc6ff',
+    accent: colors.shades.black,
+    error: colors.red.accent3
+  }
 })
