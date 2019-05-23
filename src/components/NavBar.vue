14a2d42f657d12612eb9b095297580f11d3c313c
kgoel085 <kgoel085@gmail.com>
2019-05-24 00:59:10 +0530

    Added req loading button Now sidebar is now state based

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,25 @@
 <template>
     <nav>
-        <v-toolbar app dense fixed>
+        <v-toolbar app dense fixed flat>
+            <!-- Sidebar / Navbar toggle -->
             <v-toolbar-side-icon @click="showNav = !showNav" :disabled="!apiKey"></v-toolbar-side-icon>
+
+            <!-- Title -->
             <v-toolbar-title class="headline text-uppercase">
                 <span class="primary--text">VUE </span>
                 <span class="font-weight-light secondary--text">SPLASH X</span>
             </v-toolbar-title>
+
+            <!-- Request Loader -->
+            <v-btn color="primary" flat :loading="true" v-if="reqLoader" />
+
             <v-spacer></v-spacer>
         </v-toolbar>
 
+        <!-- Side Drawer -->
         <v-navigation-drawer app v-model="showNav" class="primary">
             <v-list>
+                <!-- Logo -->
                 <v-list-tile class="pa-5">
                     <v-layout align-center justify-center row fill-height>
                         <v-flex class="text-xs-center">
@@ -21,6 +30,7 @@
                     </v-layout>
                 </v-list-tile>
                 
+                <!-- Nav items -->
                 <v-list-tile v-for="item in listItems" :key="item.title" router :to="item.path" :disabled="!apiKey">
                     <v-list-tile-actions>
                         <v-icon left color="white">
@@ -40,12 +50,28 @@
 export default {
     data(){
         return {
-            showNav: false,
+            // List of items to show in navbar
             listItems:[
                 {icon: 'dashboard', title: 'Home', path: '/'},
                 {icon: 'category', title: 'Collections', path: '/collections'}
             ]
         }
+    },
+    computed:{
+        // Controls the navbar
+        showNav:{
+            get(){
+                return (this.$store.state.showSideBar) ? this.$store.state.showSideBar : false;
+            },
+            set(val){
+                this.$store.commit('setSideBar', val);
+            }
+        },
+
+        //Manages request loader
+        reqLoader(){
+            return this.$store.state.reqLoading;
+        },
     }
 }
 </script>
