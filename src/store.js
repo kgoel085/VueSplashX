14f65c11c263aedc4450441339102fe92a449842
kgoel085 <kgoel085@gmail.com>
2019-05-24 00:55:32 +0530

    Removed extra comments

--- src/store.js
+++ src/store.js
@@ -6,7 +6,6 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     // Stores the API token 
-    // token: localStorage.getItem(process.env.VUE_APP_TOKEN_KEY),
     token: null,
 
     // Stores the error messages
@@ -32,7 +31,6 @@ export default new Vuex.Store({
 
     // Removes the current API token value and set error msg, if received
     removeToken(state, msg){
-      //localStorage.removeItem(process.env.VUE_APP_TOKEN_KEY);
       state.token = null;
 
       if(msg) state.tokenErr = msg;
