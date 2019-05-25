d483b6b36a369c5b3b040233457b810f4bfd3dc4
kgoel085 <kgoel085@gmail.com>
2019-05-26 01:18:48 +0530

    Added a var to manage diff. aspects of images

--- src/main.js
+++ src/main.js
@@ -39,6 +39,53 @@ Vue.mixin({
     apiKey(){
       if(store.state.token) return store.state.token;
       return false;
+    },
+
+    // Global obj to configure image related properties
+    dimensionObj(){
+      let mainObj = this.$vuetify.breakpoint;
+      let returnVal = {
+        height: 0,
+        size: 12,
+        breakpoint: mainObj.name,
+
+        // full - Returns img with max resolution
+        // regular - Returns img with width of 1080p
+        // small - Returns img with width of 400p
+        // thumb - Returns img with width of 200p
+        // raw - Returns raw img that can be altered with custom dimensions
+        imgType: 'small'
+      };
+      
+      // Image height
+      if(mainObj.height) returnVal.height = mainObj.height;
+
+      // Manage size & breakpoint
+      switch(mainObj.name){
+        
+        // xs - Less than 600 px
+        // sm - Less than 960 px
+        // md - Less than 1264 px
+        // lg - Less than 1904 px
+        // xl - Greater than 1904 px
+
+        case 'xs':
+        case 'sm':
+
+        break;
+
+        case 'md':
+        case 'lg':
+          returnVal.size = 4;
+        case 'xl':
+          returnVal.size = 4;
+          returnVal.height = returnVal.height / 2;
+          returnVal.imgType = 'full';
+        break;
+      }
+
+
+      return returnVal;
     }
   }
 
