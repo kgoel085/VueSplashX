66c18f2f1f47bf0d10a73e39d65c7dbdd74fea86
kgoel085 <kgoel085@gmail.com>
2019-06-09 22:23:48 +0530

    Added tag components

---
+++ src/components/Tag.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-tabs
+        show-arrows
+        class="secondary"
+        v-if="data"
+    >
+        <v-tabs-slider color="primary"></v-tabs-slider>
+
+        <v-tab
+        v-for="(tag, indx) in data"
+        :key="indx"
+        >
+            {{ tag.title }}
+        </v-tab>
+    </v-tabs>
+    <Loader v-else />
+</template>
+
+<script>
+import Loader from '../components/Loader';
+
+export default {
+    data(){
+        return {
+            data: (this.obj.length > 0) ? this.obj : null
+        }
+    },
+    components:{
+        Loader
+    },
+    props:{
+        obj:{
+            type: Array,
+            default: []
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
