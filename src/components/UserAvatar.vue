70160804472de0a6c0c790551fea14aa54a6345e
kgoel085 <kgoel085@gmail.com>
2019-06-09 22:24:25 +0530

    Updated layout

--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -2,16 +2,32 @@
     <v-list-tile left class="grow usrAvataR">
         <template v-if="data">
             <!-- Show user with name -->
-            <v-chip color="teal" text-color="white" @click.stop="showUser = !showUser">
-                <v-avatar :size="userChipSize">
-                    <img :src="userImg" :alt="data.name">
-                </v-avatar>
+            <v-list-tile-avatar text-color="white" @click.stop="showUser = !showUser" :size="userChipSize">
+                <img :src="userImg" :alt="data.name">
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
                 <v-slide-x-transition>
-                    <span v-if="showUser">
-                        {{ data.name }}
-                    </span>
+                    <v-chip class="font-weight-bold text-capitalize white--text" color="secondary" v-show="showUser">
+                        <span>{{ '@'+data.name }}</span>
+                    </v-chip>
                 </v-slide-x-transition>
-            </v-chip>
+
+                <v-slide-y-transition>
+                    <v-chip class="font-weight-bold text-capitalize" v-if="picDescription">
+                        <span class="text-truncate strictDescription">{{ picDescription }}</span>
+                    </v-chip>
+                </v-slide-y-transition>
+            </v-list-tile-content>
+            <v-spacer></v-spacer>
+            <v-layout align-center justify-end>
+                <v-slide-x-reverse-transition>
+                    <v-chip flat light color="white--text red" v-show="showUser && obj.hasOwnProperty('likes')">
+                        <v-icon left>favorite</v-icon>
+                        <span right class="subheading mr-2 white--text font-weight-heavy">{{ obj.likes }}</span>
+                    </v-chip>
+                </v-slide-x-reverse-transition>
+            </v-layout>
         </template>
         <Loader v-else></Loader>
     </v-list-tile>
@@ -23,8 +39,8 @@ import Loader from '../components/Loader';
 export default {
     data(){
         return{
-            data: (Object.keys(this.obj).length > 0) ? this.obj : null,
-            userChipSize: (this.avatarSize) ? this.avatarSize : 200,
+            data: (Object.keys(this.obj).length > 0 && this.obj.hasOwnProperty('user')) ? this.obj.user : null,
+            userChipSize: (this.avatarSize) ? this.avatarSize : 50,
             userIntervalObj: null,
             userHideInterval:3000,
             usrVisible: (this.userShow) ? this.userShow : false
@@ -72,6 +88,17 @@ export default {
 
             return returnVal;
         },
+        picDescription(){
+            var returnVal = null;
+            if(!this.showDescription) return returnVal; 
+
+            if(this.obj.description) returnVal = this.obj.description;
+            if(!returnVal && this.obj.alt_description) returnVal = this.obj.alt_description;
+
+            if(returnVal && returnVal.length > 50) returnVal = returnVal.substring(0, 50)+'...';
+
+            return returnVal;
+        }
     },
     props:{
         obj:{
@@ -84,7 +111,11 @@ export default {
         },
         avatarSize:{
             type: Number,
-            default: 200
+            default: 50
+        },
+        showDescription:{
+            type: Boolean,
+            default: false
         }
     }
 }
@@ -95,4 +126,9 @@ export default {
         height: 52px !important;
         width: 52px !important;
     } */
+    .strictDescription{
+        white-space: nowrap;
+        overflow: hidden;
+        max-width:90%
+    }
 </style>
