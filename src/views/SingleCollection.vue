e95e4a8f37d719c649573fc7c41f413c61cf0eb0
= <=>
2019-06-13 12:54:53 +0530

    Updated layout Added events / animations for cover photo Fixed: Fetching collection photos

--- src/views/SingleCollection.vue
+++ src/views/SingleCollection.vue
@@ -3,7 +3,24 @@
         <v-container fluid class="pa-1" :disabled="!apiKey">
             <v-layout row wrap v-if="data !== null">
                 <v-flex xs12 >
-                    
+                    <v-parallax :src="coverPhoto.img" v-if="coverPhoto.img" :alt="(coverPhoto.alt) ? coverPhoto.alt : ''" :height="dimensionObj.origHeight">
+                        <v-layout align-center column justify-center>
+                            <h1 class="display-2 font-weight-thin mb-3">{{ data.title }}</h1>
+                            <h4 class="subheading">
+                                <v-chip v-if="data.user.name">
+                                    <v-avatar class="primary white--text">@</v-avatar>
+                                    {{ data.user.name }}
+                                </v-chip>
+
+                                <v-chip v-if="data.total_photos">
+                                    <v-avatar class="secondary white--text">
+                                        <v-icon>collections</v-icon>
+                                    </v-avatar>
+                                    {{ data.total_photos }}
+                                </v-chip>
+                            </h4>
+                        </v-layout>
+                    </v-parallax>
                 </v-flex>
 
                 <v-flex xs12 v-if="picData.length > 0">
@@ -28,6 +45,8 @@ export default {
         return {
             data:null,
             picData: [],
+            blurFunction: null,
+            scrollFunction: null,
             params: {
                 page: 1,
                 per_page: 15
@@ -42,6 +61,27 @@ export default {
         queryId(){
             if(this.$route.params.id) return this.$route.params.id;
             return null;
+        },
+        coverPhoto(){
+            let returnVal = {
+                img: null,
+                alt: null
+            };
+
+            if(this.data && this.data.hasOwnProperty('cover_photo')){
+                if(this.data.cover_photo.urls[this.dimensionObj.imgType]){
+                    returnVal['img'] = this.data.cover_photo.urls[this.dimensionObj.imgType];
+
+                    // Bind blur scroll event 
+                    setTimeout(() => {
+                        this.setBlur();
+                    }, 1000);
+                    
+                }
+                if(this.data.cover_photo.alt_description) returnVal['alt'] = this.data.cover_photo.alt_description;
+            }
+
+            return returnVal;
         }
     },
     methods:{
@@ -68,7 +108,7 @@ export default {
         getCollectionPhotos(){
             if(!this.queryId) return false;
 
-             axios.get('/collections/'+this.queryId+'/photos').then(resp => {
+             axios.get('/collections/'+this.queryId+'/photos', {params: this.params}).then(resp => {
                 let vm = this;
                 let data = resp.data.success.data;
 
@@ -86,10 +126,34 @@ export default {
             }).catch(err => {
                this.$store.commit('setApiErr', err.message);
             });
+        },
+
+        // Set blur for Hero image on scroll
+        setBlur(){
+            let elem = document.querySelector('img.v-parallax__image');
+            this.blurFunction = () => {
+                let origScroll = (window.scrollY > 50) ? (window.scrollY / 100) : 0;
+                elem.style.filter = `blur(${origScroll}px)`;
+            }
+
+            window.addEventListener('scroll', this.blurFunction);
         }
     },
     mounted(){
-        if(this.queryId && this.apiKey) this.getCollectionDetails();
+        if(this.queryId && this.apiKey){
+            this.getCollectionDetails();
+
+            this.scrollFunction = () => {
+                if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !this.tokenExpired) this.getCollectionPhotos();
+            };
+
+            // On scroll, call next batch
+            window.addEventListener('scroll', this.scrollFunction);
+        }
+    },
+    beforeDestroy(){
+        if(this.scrollFunction) window.removeEventListener('scroll', this.scrollFunction);
+        if(this.blurFunction) window.removeEventListener('scroll', this.blurFunction);
     }
 }
 </script>
