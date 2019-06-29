f71cd5e54023d87998edcd5cb6b0e1bbc8016d07
kgoel085 <kgoel085@gmail.com>
2019-06-30 03:00:51 +0530

    Moved login method to helper

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,5 @@
 import Axios from 'axios';
 import store from './store';
-import router from './router';
 
 let helper = {
     cookieVal: false,
@@ -16,7 +15,7 @@ let helper = {
     // Generates token if expired or not available
     generateToken: async function(nxtFunc){
         if(this.checkCookie() && !nxtFunc) return true;
-        let loginDt = {email: process.env.VUE_APP_EMAIL, password: process.env.VUE_APP_PASSWORD, account: process.env.VUE_APP_ACCOUNT};
+        let loginDt = {account: process.env.VUE_APP_ACCOUNT};
         await Axios.post('/generateToken', loginDt).then(resp => {
             if(resp.data.success){
                 let tokenVal = this.getCookie(process.env.VUE_APP_TOKEN_KEY);
@@ -31,19 +30,50 @@ let helper = {
         });
     },
 
-    checkCookie: function(){
+    // Checks whether cookie is available or not
+    checkCookie: function(call = false){
         // Check for token
+        let cName = (call === false) ? process.env.VUE_APP_TOKEN_KEY : process.env.VUE_APP_LOGIN_KEY;
+        let setC = (call === false) ? 'setToken' : 'setLogin';
+
         let usrToken = (store.state.token) ? store.state.token : false;
-        let cook = this.getCookie(process.env.VUE_APP_TOKEN_KEY);
+        let cook = this.getCookie(cName);
 
         if(cook && !usrToken){
-            store.commit('setToken', cook);
+            store.commit(setC, cook);
             return true;
         }
 
-        store.commit('setToken', null);
+        store.commit(setC, null);
         return false;
-    }
+    },
+
+    // Initiaite the login process
+    initLogin: function(){
+        Axios.get('/initLogin').then(resp => {
+          let data = (resp.hasOwnProperty('data') && resp.data.hasOwnProperty('success')) ? resp.data.success : false;
+          if(data){
+            // Redirect URL
+            let port = (window.location.port) ? window.location.port : 80;
+            let loginUrl = window.location.protocol+'//'+window.location.hostname+':'+port+'/login';
+            data['redirect_uri'] = loginUrl;
+  
+            let params = [];
+            for(let key of Object.keys(data)){
+                let str = ((key == 'cd') ? 'client_id' : key) +'='+data[key];
+                params.push(str);
+            }
+  
+            let scopes = ['public','read_user','read_photos','read_collections'].join('+');
+  
+            params = (params.length > 0) ? '?'+params.join('&') : '?';
+  
+            let newWindow = window.open('https://unsplash.com/oauth/authorize'+params+'&scope='+scopes, 'vuesplashlog','_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+          }
+        }).catch(err => {
+          store.commit('setApiErr', err.message);
+        });
+      }
 };
 
 export default helper;
